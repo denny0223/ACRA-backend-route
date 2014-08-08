6ebaba67ae135ca1314b307951f4dedc2d712f13
Denny Huang <denny0223@gmail.com>
2014-08-08 14:33:47 +0800

    Support send to multi backends

--- index.php
+++ index.php
@@ -7,17 +7,24 @@
 
 // curl -v -A "Android" -H "Content-Type: application/x-www-form-urlencoded" -H"Accept: text/html,application/xml,application/json,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" -H"Connection: Keep-Alive" --data-binary @data.txt http://your.backend.host/acra/
 
-    $host = 'http://your.backend.host/acra/';
+    $hosts = array(
+      'http://your.backend.host1/acra/',
+      'http://your.backend.host2/acra/'
+    );
 
-    curl_setopt($ch, CURLOPT_URL, $host);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Android');
-    $header[] = 'Connection: Keep-Alive';
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+    foreach($hosts as $host) {
+        $ch = curl_init();
 
-    curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL, $host);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Android');
+        $header[] = 'Connection: Keep-Alive';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+
+        curl_exec($ch);
+    }
 
     foreach($_POST as $key => $value) {
         $reportLine = $key." = ".$value."\n";
